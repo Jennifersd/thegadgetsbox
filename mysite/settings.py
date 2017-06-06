5ed28727c16bd0e1e2973cae67dad5b45186780c
jennifersd <jennifersduenas@gmail.com>
2017-06-06 22:57:25 +0200

    add social footer

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2*@(*8dv08q8vbln@%d9=*xz0#ia(@6(u8ioof_#epk(7-ph4f'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['thegadgetsbox.com' , '127.0.0.1' , '178.62.125.104' , '10.211.55.4']
+ALLOWED_HOSTS = ['thegadgetsbox.com' , '127.0.0.1' , '178.62.125.104' , '10.211.55.4' , '10.211.55.5']
 
 
 # Application definition
